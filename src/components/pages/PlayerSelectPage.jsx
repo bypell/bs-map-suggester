b6e02aa84ab3b1daae64d0ee22d00378190a81e1
bypell <ippleac@gmail.com>
2024-12-18 19:35:20 -0500

    undid background because it looks ugly

--- src/components/pages/PlayerSelectPage.jsx
+++ src/components/pages/PlayerSelectPage.jsx
@@ -5,13 +5,11 @@ import Header from "../Header.jsx";
 import LoaderButton from "../LoaderButton.jsx";
 import PlayerIdInput from "../PlayerIdInput.jsx";
 import { getMapSuggestionsForUser } from '../../services/suggestMapsService.js';
-import '../../index.css';
 
 export default function PlayerSelectPage() {
     const [playerId, setPlayerId] = useState('');
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
-    const [expand, setExpand] = useState(false);
     const [error, setError] = useState(null);
 
     async function handleClick() {
@@ -21,10 +19,7 @@ export default function PlayerSelectPage() {
                 await getMapSuggestionsForUser(playerId);
             setLoading(false);
             if (suggestions) {
-                setExpand(true);
-                setTimeout(() => {
-                    navigate(`/suggestions/${playerId}`, { state: { suggestions } });
-                }, 400);
+                navigate(`/suggestions/${playerId}`, { state: { suggestions } });
             } else {
                 console.error("No suggestions found.");
             }
@@ -34,20 +29,17 @@ export default function PlayerSelectPage() {
     }
 
     return (
-        <div className="checkerboard h-screen w-screen relative text-white bg-black flex flex-col justify-center items-center overflow-hidden">
-            <div className={`flex flex-col items-center justify-center px-16 py-32 bg-dark rounded-3xl shadow-lg transition-all duration-300 ${expand ? 'w-screen h-screen' : 'w-[60rem] h-[25rem]'}`}>
-                <div className={`flex flex-col items-center transition-opacity duration-300 ${expand ? 'opacity-0' : 'opacity-100'}`}>
-                    <Header />
-                    <div className="flex flex-row">
-                        <PlayerIdInput onValidPlayerEntered={(id) => setPlayerId(id)} disabled={loading} />
-                        <LoaderButton text="Get Suggestions" onClick={handleClick} disabled={loading} />
-                    </div>
-                    <div className={`flex flex-row mt-10`}>
-                        {loading && <Spinner />}
-                        {error && <div className="text-red-500 mt-4">Error: {error.message}</div>}
-                    </div>
+        <div className="checkerboard h-screen w-screen relative text-white bg-dark flex flex-col justify-center items-center overflow-hidden">
+            <div className={`flex flex-col items-center`}>
+                <Header />
+                <div className="flex flex-row">
+                    <PlayerIdInput onValidPlayerEntered={(id) => setPlayerId(id)} disabled={loading} />
+                    <LoaderButton text="Get Suggestions" onClick={handleClick} disabled={loading} />
                 </div>
-
+                <div className={`flex flex-row mt-10`}>
+                    {loading && <Spinner />}
+                    {error && <div className="text-red-500 mt-4">Error: {error.message}</div>}
+                </div>
             </div>
         </div>
     );
