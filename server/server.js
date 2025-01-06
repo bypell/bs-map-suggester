9af80f8c868518fece3219bd993a3f7a5d76914c
bypell <ippleac@gmail.com>
2025-01-05 23:18:33 -0500

    removed cors

--- server/server.js
+++ server/server.js
@@ -5,24 +5,24 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const allowedOrigins = [
-    'https://bypell.github.io',
-    'http://localhost:3000'
-];
+// const allowedOrigins = [
+//     'https://bypell.github.io',
+//     'http://localhost:3000'
+// ];
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     methods: ['GET'],
+//     allowedHeaders: ['Content-Type', 'Authorization']
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 app.use('/scoresaber', createProxyMiddleware({
     target: 'https://scoresaber.com/api',
@@ -35,13 +35,13 @@ app.use('/scoresaber', createProxyMiddleware({
         'Accept': 'application/json'
     },
     logLevel: 'debug',
-    onProxyReq: (proxyReq, req, res) => {
-        console.log(`Proxying request to: ${proxyReq.path}`);
-    },
-    onProxyRes: (proxyRes, req, res) => {
-        proxyRes.headers['Access-Control-Allow-Origin'] = allowedOrigin;
-        console.log(`Received response with status: ${proxyRes.statusCode}`);
-    },
+    // onProxyReq: (proxyReq, req, res) => {
+    //     console.log(`Proxying request to: ${proxyReq.path}`);
+    // },
+    // onProxyRes: (proxyRes, req, res) => {
+    //     proxyRes.headers['Access-Control-Allow-Origin'] = allowedOrigin;
+    //     console.log(`Received response with status: ${proxyRes.statusCode}`);
+    // },
 }));
 
 app.listen(PORT, () => {
