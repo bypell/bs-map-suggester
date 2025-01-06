9b243eab89550fb21da6f90d96d4927378091f9f
bypell <ippleac@gmail.com>
2025-01-05 23:16:40 -0500

    removed proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      // '/api': 'https://scoresaber.com',
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '/scoresaber')
-      }
-    },
+    // proxy: {
+    // '/api': 'https://scoresaber.com',
+    // '/api': {
+    //   target: 'http://localhost:3000',
+    //   changeOrigin: true,
+    //   rewrite: path => path.replace(/^\/api/, '/scoresaber')
+    // }
+    // },
   },
   plugins: [react(),],
   base: '/bs-map-suggester/',
