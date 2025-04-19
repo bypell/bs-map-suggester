b8980eae432e360ac319c3f4874a00576b33ec2d
bypell <ippleac@gmail.com>
2025-04-19 13:46:48 -0400

    I should read the docs more often instead of relying on chatgpt for fixes lol

--- client/src/hooks/useSuggestionAudioPlayer.js
+++ client/src/hooks/useSuggestionAudioPlayer.js
@@ -10,26 +10,13 @@ export function useSuggestionAudioPlayer() {
     const play = async (songUrl, id) => {
         sharedAudio.pause();
         setCurrentlyPlaying(id);
-        await delay(100);
 
         if (sharedAudio.src !== songUrl) {
             sharedAudio.src = songUrl;
-
-            await new Promise((resolve) => {
-                const onLoadedData = () => {
-                    sharedAudio.removeEventListener('loadeddata', onLoadedData);
-                    resolve();
-                };
-                sharedAudio.addEventListener('loadeddata', onLoadedData);
-                sharedAudio.load();
-            });
         }
 
         sharedAudio.volume = 0.2;
-
-        sharedAudio.play().catch(err => {
-            console.warn('Audio play error:', err);
-        });
+        sharedAudio.autoplay = true;
     };
 
     const pause = () => {
