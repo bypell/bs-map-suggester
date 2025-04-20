481b7352850cb701f83ee302a1570fe06b2a7ba2
bypell <ippleac@gmail.com>
2025-04-20 18:41:57 -0400

    no more pointer cursor on song playing overlay

--- client/src/components/SongPlayingOverlay.jsx
+++ client/src/components/SongPlayingOverlay.jsx
@@ -17,7 +17,7 @@ function SongPlayingOverlay({ songUrl, id, play, pause, currentlyPlaying }) {
     return (
         <div
             onClick={togglePlayPause}
-            className={`absolute inset-0 w-full h-full flex items-center justify-center hover:cursor-pointer hover:opacity-100 bg-black bg-opacity-50 ${currentlyPlaying === id ? 'opacity-100' : 'opacity-0'
+            className={`absolute inset-0 w-full h-full flex items-center justify-center hover:opacity-100 bg-black bg-opacity-50 ${currentlyPlaying === id ? 'opacity-100' : 'opacity-0'
                 }`}
         >
             <i className={`fas fa-${currentlyPlaying === id ? 'pause' : 'play'} text-2xl text-white`}></i>
