444bc09bc0bb799167bce56ee04518cae0b0d6e0
bypell <ippleac@gmail.com>
2025-04-19 22:48:56 -0400

    wasnt even needed bruh

--- client/src/hooks/useSuggestionAudioPlayer.js
+++ client/src/hooks/useSuggestionAudioPlayer.js
@@ -5,23 +5,7 @@ const sharedAudio = new Audio();
 export function useSuggestionAudioPlayer() {
     const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
 
-    const pauseAndWaitUntilActuallyPausedForRealBecauseTheresAnAudioPopOtherwise = () => {
-        return new Promise((resolve) => {
-            if (sharedAudio.paused) {
-                resolve();
-            } else {
-                const onPause = () => {
-                    sharedAudio.removeEventListener('pause', onPause);
-                    resolve();
-                };
-                sharedAudio.addEventListener('pause', onPause);
-                sharedAudio.pause();
-            }
-        });
-    };
-
     const play = async (songUrl, id) => {
-        await pauseAndWaitUntilActuallyPausedForRealBecauseTheresAnAudioPopOtherwise();
         setCurrentlyPlaying(id);
 
         if (sharedAudio.src !== songUrl) {
