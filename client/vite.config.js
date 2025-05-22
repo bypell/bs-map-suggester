a70cbf0f87988e21f52cfde669810e8dfe1d081e
bypell <ippleac@gmail.com>
2025-05-21 21:48:49 -0400

    skgjhskgs

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactScan from '@pivanov/vite-plugin-react-scan';
 
 const ReactCompilerConfig = {
   target: '19' // '17' | '18' | '19'
 };
 
 // https://vite.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      // '/api': 'https://scoresaber.com',
-    },
-  },
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["babel-plugin-react-compiler", ReactCompilerConfig],
-        ],
+export default defineConfig(({ mode }) => {
+  return {
+    server: {
+      proxy: {
+        // '/api': 'https://scoresaber.com',
       },
-    }),
-  ],
-  base: '/bs-map-suggester/',
-})
+    },
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ["babel-plugin-react-compiler", ReactCompilerConfig],
+          ],
+        },
+      }),
+      reactScan({
+        enable: process.env.NODE_ENV === 'development'
+      }),
+    ],
+    base: '/bs-map-suggester/',
+  };
+});
