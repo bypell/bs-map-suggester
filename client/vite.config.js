eddcb966ceffc120921b23a97250daf9e208d5fe
bypell <ippleac@gmail.com>
2025-05-21 20:25:14 -0400

    added react compiler

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const ReactCompilerConfig = {
+  target: '19' // '17' | '18' | '19'
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   server: {
@@ -8,6 +12,14 @@ export default defineConfig({
       // '/api': 'https://scoresaber.com',
     },
   },
-  plugins: [react(),],
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          ["babel-plugin-react-compiler", ReactCompilerConfig],
+        ],
+      },
+    }),
+  ],
   base: '/bs-map-suggester/',
 })
